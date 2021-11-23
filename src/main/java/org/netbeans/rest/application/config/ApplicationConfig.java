9a2993d85fe304864510033f9d2b15121598cf26
CesarTllez <lcesartellezl@gmail.com>
2021-11-23 13:09:20 -0500

    @Provider en CORS

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(co.edu.unicundi.discotiendaapiwar.CORS.CorsFilter.class);
         resources.add(co.edu.unicundi.discotiendaapiwar.controlador.ArtistaControlador.class);
         resources.add(co.edu.unicundi.discotiendaapiwar.controlador.CancionControlador.class);
         resources.add(co.edu.unicundi.discotiendaapiwar.controlador.DiscoController.class);
